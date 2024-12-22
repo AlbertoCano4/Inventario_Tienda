e0dc6c0918b372c606fe985d042e7bf32693cf58
AlbertoCano4 <alberto.cano@cunef.edu>
2024-12-22 14:00:27 +0100

    Modificando .cpp

--- PROYECTO GRUPAL/Accesorio.cpp
+++ PROYECTO GRUPAL/Accesorio.cpp
@@ -1,7 +1,23 @@
-//
-//  Accesorio.cpp
-//  PROYECTO GRUPAL
-//
-//  Created by Alberto Cano Moreno on 14/11/24.
-//
+#include "accesorio.hpp"
+#include <iostream>
 
+using namespace std;
+
+// Constructor
+Accesorio::Accesorio(string cod, float precioV, int cant, Genero gen, Temporada temp)
+: Producto(cod, precioV, cant, gen), temporada(temp) {}
+// Getters y Setters
+
+Temporada Accesorio::getTemporada() const{
+    return temporada;
+}
+
+void Accesorio::setTemporada(Temporada temp){
+    temporada = temp;
+}
+
+// Implementación del método mostrarInformacion
+void Accesorio::mostrarInformacion() {
+    Producto::mostrarInformacion();  // Llamamos al método de la clase padre
+    cout << "Temporada: " << static_cast<int>(temporada) << endl;
+}
